114fc58fdf2303cd633425d4f69834a1309eba61
riyandharne-123 <60026968+riyandharne-123@users.noreply.github.com>
2020-06-14 14:33:18 +0530

    Updates

---
+++ precache-manifest.317a0c32cc169cf117210a52e9a7962f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d267cc6d0cd74cc999eb32dc8405ca0",
+    "url": "/Movie-App-React/index.html"
+  },
+  {
+    "revision": "a2fb111db55418ba6bef",
+    "url": "/Movie-App-React/static/css/main.e38274fb.chunk.css"
+  },
+  {
+    "revision": "da2ebea44686388b1435",
+    "url": "/Movie-App-React/static/js/2.a37b4712.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movie-App-React/static/js/2.a37b4712.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2fb111db55418ba6bef",
+    "url": "/Movie-App-React/static/js/main.02ceb398.chunk.js"
+  },
+  {
+    "revision": "8c27d1b7bb8f4ac8af21",
+    "url": "/Movie-App-React/static/js/runtime-main.58c143cd.js"
+  }
+]);
